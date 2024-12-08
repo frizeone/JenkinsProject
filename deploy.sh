20fddc00f44d04b4987bab083bef6e0537fdc34d
Скрипников Ростислав Станиславович (4111531) - ООО ГК <rsskripnikov@dev.vtb.ru>
2024-12-09 01:59:59 +0300

    update jenkins file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
-#!/bin/bash
+#! /bin/bash
 echo "Deploying the application..."
-java target/JenkinsProject-0.0.1-SNAPSHOT.jar
+
+# Печатаем текущую директорию
+echo "Current directory: $(pwd)"
+
+# Пытаемся найти JAR-файл в директории target
+JAR_FILE=$(find target -name "*.jar")
+
+if [ -z "$JAR_FILE" ]; then
+    echo "JAR file not found!"
+    exit 1
+else
+    echo "Found JAR file: $JAR_FILE"
+    java -jar "$JAR_FILE"  # Запускаем приложение
+fi
